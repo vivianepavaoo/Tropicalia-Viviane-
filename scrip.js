7809082494e3463659562981c644b11290195ad1
vivianepavaoo <146456646+vivianepavaoo@users.noreply.github.com>
2025-04-29 08:25:00 -0300

    29/04

--- scrip.js
+++ scrip.js
@@ -31,3 +31,8 @@ document.addEventListener('DOMContentLoaded', function(){
     document.body.classList.toggle('alto-contraste');
     })
     });
+
+    ScrollReveal().reveal('#inicio',{delay: 500});
+    ScrollReveal().reveal('#galeria',{delay: 500});
+    ScrollReveal().reveal('#tropicalia',{delay: 500});
+    ScrollReveal().reveal('#contato',{delay: 500});
