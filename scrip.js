381e3e9a4e65cd779cf420f7de760c5eaaf8e3c3
vivianepavaoo <146456646+vivianepavaoo@users.noreply.github.com>
2025-04-29 08:11:35 -0300

    29/04

--- scrip.js
+++ scrip.js
@@ -1,17 +1,19 @@
 document.addEventListener('DOMContentLoaded', function(){
 
-    const botaoDeAcessibilidade = document.getElementsById('botao-acessibilidade');
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
     const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
-
-    botaoDeAcessibilidade.addEventListener('click' ,function(){
-        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
-        opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+    
+    botaoDeAcessibilidade.addEventListener('click',function(){
+    botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+    opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
     })
-
-
-
+    
+    
+    
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+    
+    const alternaContraste = document.getElementById('alterna-contraste');
     
     let tamanhoAtualFonte = 1;
     
@@ -24,4 +26,8 @@ document.addEventListener('DOMContentLoaded', function(){
     tamanhoAtualFonte -= 0.1;
     document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
     });
+    
+    alternaContraste.addEventListener('click', function(){
+    document.body.classList.toggle('alto-contraste');
+    })
     });
